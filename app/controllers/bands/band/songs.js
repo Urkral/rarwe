91c643d5196b4fff0b0b57df47fa8383c9436f02
Urkral <esteban.dutrannois@outlook.com>
2022-09-14 16:25:43 +0200

    .

---
+++ app/controllers/bands/band/songs.js
@@ -0,0 +1,78 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import { capitalize } from 'rarwe/helpers/capitalize';
+
+export default class BandsBandSongsController extends Controller {
+  @tracked showAddSong = true;
+  @tracked title = '';
+  @tracked sortBy = 'title';
+  @tracked searchTerm = '';
+
+  get matchingSongs() {
+    let searchTerm = this.searchTerm.toLowerCase();
+    return this.model.songs.filter((song) => {
+      return song.title.toLowerCase().includes(searchTerm);
+    });
+  }
+  get newSongPlaceholder() {
+    let bandName = this.model.name;
+    return `New ${capitalize([bandName])} song`;
+  }
+
+  get hasNoTitle() {
+    return !this.title;
+  }
+  @service catalog;
+  get sortedSongs() {
+    let sortBy = this.sortBy;
+    let isDescendingSort = false;
+    if (sortBy.charAt(0) === '-') {
+      sortBy = this.sortBy.slice(1);
+      isDescendingSort = true;
+    }
+    return this.matchingSongs.sort((song1, song2) => {
+      if (song1[sortBy] < song2[sortBy]) {
+        return isDescendingSort ? 1 : -1;
+      }
+      if (song1[sortBy] > song2[sortBy]) {
+        return isDescendingSort ? -1 : 1;
+      }
+      return 0;
+    });
+  }
+  @action
+  async updateRating(song, rating) {
+    song.rating = rating;
+    this.catalog.update('song', song, { rating });
+  }
+
+  @action
+  updateTitle(event) {
+    this.title = event.target.value;
+  }
+
+  @action
+  async saveSong() {
+    let song = await this.catalog.create(
+      'song',
+      { title: this.title },
+      { band: { data: { id: this.model.id, type: 'bands' } } }
+    );
+    this.model.songs = [...this.model.songs, song];
+    this.title = '';
+    this.showAddSong = true;
+  }
+
+  @action
+  updateSearchTerm(event) {
+    this.searchTerm = event.target.value;
+  }
+
+  @action
+  cancel() {
+    this.title = '';
+    this.showAddSong = true;
+  }
+}
